2fa529267cd1a4f6e2e7b9709d67d92aa0180cd2
Lucas Bastos <lfsouzabastos@gmail.com>
2025-04-26 21:01:40 -0300

    Update bank.py

--- week1-conditionals/2-bank/bank.py
+++ week1-conditionals/2-bank/bank.py
@@ -3,10 +3,9 @@ def main():
     print(f"${greetings}")
 
 def checkGreetings(str):
-    # If a greeting start with "hello" = return 0
-    if str.count("hello", 0, 5) == 1:
+    if str.lower().count("hello", 0, 5) == 1:
         return 0
-    elif str.count("h", 0, 1) == 1:
+    elif str.lower().count("h", 0, 1) == 1:
         return 20
     else:
         return 100
